ff8b7c41b1905bd047a7e9519fbbf348e1cded56
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-04-02 16:42:04 -0600

    adding 2nd sltn

---
+++ ArraysAndHashing/src/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,13 @@
+public class RemoveDuplicateFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int index = 1; // the first element will always be unique.
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                nums[index] = nums[i + 1];
+                index++;
+            }
+        }
+        return index;
+    }
+}

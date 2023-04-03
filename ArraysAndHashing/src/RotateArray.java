201f259bd1d24dda7c94ec527dae57b17bc9bf54
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-04-03 15:09:51 -0600

    adding 3rd qst

---
+++ ArraysAndHashing/src/RotateArray.java
@@ -0,0 +1,24 @@
+public class RotateArray {
+     /*
+     Original List                   : 1 2 3 4 5 6 7
+     After reversing all numbers     : 7 6 5 4 3 2 1
+     After reversing first k numbers : 5 6 7 4 3 2 1
+     After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result*/
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

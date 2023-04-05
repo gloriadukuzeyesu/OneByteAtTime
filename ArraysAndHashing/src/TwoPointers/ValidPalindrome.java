3aa23d19923f351f5584c08ce576071899c0a647
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-04-05 15:35:38 -0600

    valid palindrome

--- ArraysAndHashing/src/TwoPointers/ValidPalindrome.java
+++ ArraysAndHashing/src/TwoPointers/ValidPalindrome.java
@@ -1,5 +1,30 @@
 package TwoPointers;
 
 public class ValidPalindrome {
+    //use two pointers, one from the front and another from the back
+    // those pointers will skip over the non-alphanumeric characters
+    // and only compare chars after converting them to lowercase
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        char head;
+        char tail;
+        while (i <= j) {
+            head = s.charAt(i);
+            tail = s.charAt(j);
+            if (!Character.isLetterOrDigit(head)) {
+                i++;
+            } else if (!Character.isLetterOrDigit(tail)) {
+                j--;
+            } else {
+                if (Character.toLowerCase(head) != Character.toLowerCase(tail)) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
 
+    }
 }

83133776964c793a19e4e016f0596a7691bdf9a9
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-04-26 00:00:59 -0600

    Time: 10 ms (29.09%), Space: 43.3 MB (44.32%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,33 +1,24 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j){
-            char ch1=s.charAt(i);
-            char ch2=s.charAt(j);
-            if(ch1!=ch2){
-                boolean res1=ispalindrome(s,i+1,j);
-                boolean res2=ispalindrome(s,i,j-1);
-                if(res1==true || res2==true){
-                    return true;
-                }else{
-                    return false;
-                }
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
             }
-            i++;
-            j--;
+            else return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
         }
         return true;
     }
-    
-    public boolean ispalindrome(String s,int i,int j){
-     
-        while(i<j){
-         char ch1=s.charAt(i);
-         char ch2=s.charAt(j);
-            if(ch1!=ch2) return false;
-            i++;
-            j--;
+    public boolean isPalindrome(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return false;
         }
         return true;
     }

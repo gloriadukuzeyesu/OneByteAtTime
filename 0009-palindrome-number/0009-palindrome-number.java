dd20105b1cf27df89f053e733f8fb9ec90d84db6
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-01 22:41:55 -0600

    Time: 9 ms (99.47%), Space: 42 MB (59.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
-             if(x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+       if(x<0){
+           return false;
+       }
+        int reverse = 0;
+        int orginal =x;  
+        while(orginal!=0){
+            reverse = reverse*10+orginal%10;
+            orginal/=10;
         }
-
-        int revertedNumber = 0;
-        while(x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
-            x /= 10;
-        }
-
-        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
-        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
-        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
-        return x == revertedNumber || x == revertedNumber/10;
-        
+        return reverse == x;
     }
 }

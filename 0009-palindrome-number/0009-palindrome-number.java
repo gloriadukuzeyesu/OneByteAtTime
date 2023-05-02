0f7a504ae3ae58a0498ce390718e4bb3917802b9
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-01 22:46:26 -0600

    Time: 9 ms (99.47%), Space: 42.2 MB (49.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) { 
         // when x is negative, we won't enter while loop. 
+        if(x < 0) {
+            return false;
+        }
         int reverse = 0;
         int orginal =x;  
-        while(orginal > 0){ 
+        while(orginal != 0){ 
             reverse = reverse*10+orginal%10;
             orginal/=10;
         }

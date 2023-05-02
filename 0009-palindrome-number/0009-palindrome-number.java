f038527a0acbf90f7d942df076cc4a940d883418
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-01 22:43:55 -0600

    Time: 9 ms (99.47%), Space: 42.1 MB (59.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,11 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-       if(x<0){
-           return false;
-       }
+    public boolean isPalindrome(int x) { // -121 
+       // if(x<0){
+       //     return false;
+       // }
         int reverse = 0;
         int orginal =x;  
-        while(orginal!=0){
+        while(orginal > 0){ 
             reverse = reverse*10+orginal%10;
             orginal/=10;
         }

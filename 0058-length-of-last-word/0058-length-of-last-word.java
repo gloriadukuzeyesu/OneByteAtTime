a69206f38e7d862a1991e7a9f61827f2c251399a
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-08 23:45:27 -0600

    Time: 0 ms (100.00%), Space: 40.7 MB (80.41%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-
-       s =  s.trim();
-        return s.length() - s.lastIndexOf(' ') - 1;  
+        int length = 0; 
+        
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) != ' ') {
+                length++; // increment the length if a non space character is found
+            } else {
+                if(length > 0 ) return length; // if the lenght is greater than 0, it means that the last word's length has already been found. 
+            }
+        }
+        return length; 
     }
 }

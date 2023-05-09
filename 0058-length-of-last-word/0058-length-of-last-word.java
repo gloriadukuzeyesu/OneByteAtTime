9f9c2117b24c691204c23710a6e4fcbbc1b75b04
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-08 23:37:45 -0600

    Time: 0 ms (100.00%), Space: 41 MB (45.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,15 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0; 
-        
-        for(int i = s.length() - 1; i >= 0; i--) {
-            if(s.charAt(i) != ' ') {
-                length++; 
-            } else {
-                if(length > 0 ) return length;  
-                
-            }
-        }
-        return length; 
+
+       s =  s.trim();
+        return s.length() - s.lastIndexOf(' ') - 1;  
     }
 }

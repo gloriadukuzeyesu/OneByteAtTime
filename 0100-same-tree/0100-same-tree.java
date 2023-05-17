61e2e4d4681ef1de2fbbc230af0825cbbb870410
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-16 22:13:37 -0600

    Time: 0 ms (100.00%), Space: 40.4 MB (32.32%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,13 +18,11 @@ class Solution {
         if(p == null && q == null){
             return true; 
         }
-        if(p == null && q != null) {
+        
+        if(p == null || q == null) {
             return false; 
         }
         
-        if(p != null && q == null) {
-            return false;
-        }
         if(p.val != q.val) {
             return false;
         }

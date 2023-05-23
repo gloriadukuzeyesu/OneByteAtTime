fd57932d4b140d8e81122f7514171424492a23b9
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-23 14:19:27 -0600

    Time: 8 ms (43.91%), Space: 39.7 MB (33.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public int fib(int n) {
         // base case
-        if(n==0){
-           return 0; 
-        }  
-        if (n==1)  {
-            return 1; 
-        } 
+        // if(n==0){
+        //    return 0; 
+        // }  
+        // if (n==1)  {
+        //     return 1; 
+        // } 
+        
+        if(n <=1) {
+            return n; 
+        }
         // recursice case 
         return fib(n - 1) + fib(n - 2); 
     }

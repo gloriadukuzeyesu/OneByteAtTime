24ba2c9c12f51ac64e38dc15c90be75c4a1e5dd3
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-23 14:59:02 -0600

    Time: 0 ms (100.00%), Space: 39.6 MB (44.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,29 @@
 class Solution {
+        Map<Integer, Integer>cache = new HashMap<>(); 
     public int fib(int n) {
-        if(n <=1) {
-            return n;
+        if(n==1)return 1; 
+        if(n==0) return 0; 
+        if(cache.containsKey(n)) {
+            return cache.get(n); 
         }
-        int current= 0;
-        int prev1 = 1; 
-        int prev2 = 0;
         
-        for(int i = 2; i < n + 1; i++) {
-            current = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return current; 
+        int fn = fib(n - 1) + fib(n - 2); 
+        cache.put(n, fn);
+        return cache.get(n);
         
+//         if(n <=1) {
+//             return n;
+//         }
+//         int current= 0;
+//         int prev1 = 1; 
+//         int prev2 = 0;
+        
+//         for(int i = 2; i < n + 1; i++) {
+//             current = prev1 + prev2;
+//             prev2 = prev1;
+//             prev1 = current;
+//         }
+//         return current; 
+          
     }
 }

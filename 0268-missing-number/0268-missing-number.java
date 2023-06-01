3950b50073d796f5001189a3245b71c20d293a4e
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-05-31 21:36:59 -0600

    Time: 0 ms (100.00%), Space: 44.2 MB (13.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -11,23 +11,3 @@ class Solution {
         return sum2 - sum1; 
     }
 }
-
-// [0,1]
-// n = 2
-    
-// range : from 0 to 2 
-// all numbers: 0, 1, 2
-    
-// Missing: 2 
-//     return 2
-// // approach 
-    
-// sum1 
-// sum2 
-// loop thru nums
-// sum1: summing all numbers in array 
-// sum2: summing indexes
-
-// res =difference betwenn sum1 and sum2
-//  return=res 
-     

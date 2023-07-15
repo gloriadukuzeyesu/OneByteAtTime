88f82bc65acea1f338be4b4236627047ab3615cd
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-07-15 14:14:13 -0600

    Time: 0 ms (100.00%), Space: 41.1 MB (53.53%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,18 +3,11 @@ class Solution {
         int k = 0; 
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != val) {
-                int temp = nums[k]; 
                 nums[k] = nums[i]; 
-                nums[i] = temp;
                 k++; 
-            
             }
         }
-        
-        
-        
-        return k; 
-        
+        return k;    
         
     }
 }

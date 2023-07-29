d5721001b626c8407feeb73f108f96671ff755fc
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-07-29 11:32:08 -0600

    Time: 15 ms (41.15%), Space: 56.8 MB (36.08%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num); 
+        }
+        return set.size() != nums.length;     
+    }
+}

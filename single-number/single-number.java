1b7b9312a1edb7ca4dc37606a7ba25664a35b31d
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-08-09 22:47:45 -0600

    Time: 1 ms, Memory: 44.2 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-         int result = 0;
-    for(int i : nums) {
-        result ^= i;
+        int res = 0; 
+        for (int num : nums) {
+            res =  res ^ num; 
+        }
+        return res;  
     }
-    return result;
-            }
 }

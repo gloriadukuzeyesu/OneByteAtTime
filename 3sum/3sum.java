75da34d5b636ec62a1789e617b2fad5b069fddd6
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-08-17 22:00:38 -0600

    Time: 28 ms, Memory: 50.1 MB - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
-        
         for(int i = 0; i < nums.length - 2; i++) {
             if( i > 0 && nums[i] == nums[i - 1]){
                 continue;
@@ -18,8 +17,12 @@ class Solution {
                     } else {
                         list.add( Arrays.asList(nums[l], nums[r] , nums[i]));
                         l++;
+                        r--;
                         while ( l < r && nums[l] == nums[l - 1]) {
                             l++;
+                        }
+                         while (l < r && nums[r] == nums[r + 1]) {
+                            r--;
                         }
                     }
                 }

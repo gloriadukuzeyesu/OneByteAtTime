67a228e53c1c506639297256c79371d224cb5f85
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-08-18 19:46:13 -0600

    Time: 13 ms (78.29%), Space: 48.3 MB (5.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,32 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer>map = new HashMap<Integer, Integer>(); 
-        for(int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1); 
-        }
-
-        // create the bucket
-        ArrayList<Integer>[] bucket = new ArrayList[nums.length + 1];
-
-        for(int key : map.keySet()) {
-            int frequency = map.get(key); 
-            if(bucket[frequency] == null) {
-                bucket[frequency]  = new ArrayList<Integer>(); 
+        int [] arr = new int[k]; 
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        for(int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(
+            (a, b) -> a.getValue() - b.getValue()  // Compare entries by frequency
+        );
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            maxHeap.offer(entry);
+            if(maxHeap.size() > k) {
+                maxHeap.poll(); 
             }
-            bucket[frequency].add(key); 
+        }      
+        int i = 0; 
+        while (!maxHeap.isEmpty()) {
+            arr[i] = maxHeap.poll().getKey();
+            i++;
         }
-
-        // create the array for the results
-        int[] res = new int[k]; 
-        int index = 0; 
-        for(int pos = bucket.length - 1; pos >= 0 && index < k; pos--) {
-            if(bucket[pos] != null) {
-                for(int element : bucket[pos]) {
-                    res[index] = element; 
-                    index++; 
-                }
-            }
-        }
-        return res; 
+        return arr;
+     
     }
 }

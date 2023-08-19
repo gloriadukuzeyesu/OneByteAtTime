281bbcdc88108219b241abaf9f029d8c8b3bae7b
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-08-18 18:47:09 -0600

    Time: 37 ms (54.76%), Space: 54.8 MB (51.81%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,22 +1,16 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
-        
-        // Fill the min-heap with the first k elements from the array
-        for (int i = 0; i < k; i++) {
-            minHeap.offer(nums[i]);
+        // create a min heap 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        for(int i = 0; i < k ; i++) {
+            pq.add(nums[i]); 
         }
-        
-        // For the rest of the elements in the array, compare with the smallest element in the heap
-        // If the current element is larger than the smallest element in the heap, replace it
-        for (int i = k; i < nums.length; i++) {
-            if (nums[i] > minHeap.peek()) {
-                minHeap.poll();
-                minHeap.offer(nums[i]);
+        for(int i = k ; i< nums.length; i++) {
+            if(nums[i] > pq.peek()) {
+                pq.poll(); 
+                pq.add(nums[i]);
             }
         }
-        
-        // The kth largest element will be the smallest element in the min-heap
-        return minHeap.peek(); 
+        return pq.peek(); 
     }
 }

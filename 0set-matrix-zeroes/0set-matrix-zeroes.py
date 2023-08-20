0f236b5b86da8010066a1b7003497f971bcc291f
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-08-20 00:17:31 -0600

    Time: 108 ms, Memory: 13.9 MB - LeetHub

---
+++ 0set-matrix-zeroes/0set-matrix-zeroes.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False; 
+        
+        #determine which rows and columsn need to be zero 
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0: 
+                    matrix[0][c] = 0 # set the first cell on that col to 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+                        
+         # find the col and row to zero 
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+                    
+        # zero out the first row and 1 col 
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+                
+        if rowZero: 
+            for c in range(COLS):
+                matrix[0][c] = 0
+        
+                    
+
+        
+        

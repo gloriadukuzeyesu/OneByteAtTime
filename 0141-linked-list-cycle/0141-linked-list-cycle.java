fbe9b923ac64ef17141b07aa31549c3597db4623
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-08-24 16:55:20 -0600

    Time: 0 ms (100.00%), Space: 43.6 MB (57.16%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,18 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head; 
-        ListNode fast = head; 
-        // fast pointer move twice as slow. Eventually the fast pointer will catch the slow    
-        //pointer if there is a cycle. 
-
-        while(fast != null && fast.next != null) {
-            fast = fast.next.next; 
-            slow = slow.next;
-            if(fast == slow) {
-                return true; 
-            }       
+        if(head == null || head.next == null) {
+            return false;
         }
-        return false; 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 }

c0baad7869d879e6c64121bf79d75e8f5355543a
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-02 10:39:30 -0600

    Time: 19 ms (18.02%), Space: 43.8 MB (22.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false; 
-        
-        int[] store = new int[26]; 
-        for(int i = 0; i < s.length(); i++) {
-            store[s.charAt(i) -'a']++; 
-            store[t.charAt(i) - 'a']--; 
+        if(s.length() != t.length()) {
+            return false; 
         }
-        for(int n : store) if (n != 0) return false; 
-     
-        return true; 
+        Map<Character, Integer> sMap = new HashMap<>(); 
+        Map<Character, Integer> tMap = new HashMap<>(); 
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(sMap.containsKey(s.charAt(i))) {
+                // update its value 
+                int newValue = sMap.get(s.charAt(i)) + 1; 
+                sMap.put(s.charAt(i), newValue);
+            } else {
+                sMap.put(s.charAt(i), 1);
+            }  
+        }
+        
+        for(int i = 0; i < t.length(); i++) {
+            if(tMap.containsKey(t.charAt(i))) {
+                // update its value 
+                int newValue = tMap.get(t.charAt(i)) + 1; 
+                tMap.put(t.charAt(i), newValue);
+            } else {
+                tMap.put(t.charAt(i), 1);
+            }  
+        }
+        // compare two maps.
+        return sMap.equals(tMap); 
     }
 }

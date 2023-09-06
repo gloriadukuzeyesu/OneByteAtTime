17d125f7ec897dbede7f4f4f381dae7e475b87a2
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-05 21:20:07 -0600

    Time: 40 ms (51.07%), Space: 44.2 MB (82.60%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -4,33 +4,24 @@ class Solution {
         Stack<Integer> counter = new Stack<>(); 
         for(char c : s.toCharArray()) {
             if(charSt.size() > 0 && charSt.peek() == c) {
-                int curr = counter.pop(); 
-                counter.push(curr + 1); 
+                counter.push(counter.pop() + 1); 
             } else {
-                
                 counter.push(1);
                 charSt.push(c);
             }
-            
-            
             if(counter.peek() == k) {
                   charSt.pop();
                   counter.pop(); 
-                // for(int i = 0; i < k ; i++) {
-                //     charSt.pop();
-                //     counter.pop(); 
-                // }
             }
         }
         StringBuilder sb  = new StringBuilder(); 
         while(charSt.size()> 0) {
             int end = counter.pop();
             char curr = charSt.pop();
-            while(end > 0) {
+            while(end --> 0) {
                 sb.append(curr); 
-                end--;
+                //end--;
             }
-            //sb.append(charSt.pop());
         }
         return sb.reverse().toString(); 
     }

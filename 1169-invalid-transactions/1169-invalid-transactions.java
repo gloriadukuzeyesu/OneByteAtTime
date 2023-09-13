8b629651699f5efa16e97866377b8ce2eb828c16
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-12 20:53:43 -0600

    Time: 8 ms (98.89%), Space: 44.4 MB (70.83%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.java
+++ 1169-invalid-transactions/1169-invalid-transactions.java
@@ -1,60 +1,49 @@
 class Solution {
-	// store transaction in an object for ease of handling and cleaner code
-    class Transaction{
-        String name;
-        int time;
-        int amount;
-        String city;
-        public Transaction(String line){
-			// split the string and parse it
-            String[] split = line.split(",");
-            name = split[0];
-            time = Integer.parseInt(split[1]);
-            amount = Integer.parseInt(split[2]);
-            city = split[3];
-        }
-    }
     public List<String> invalidTransactions(String[] transactions) {
         List<String> invalid = new ArrayList<>();
-        Map<String, List<Transaction>> map = new HashMap<>();
-		// go through entire transaction list and create object and store in map using 'name' as key
-        for(String transaction : transactions){
-            Transaction t = new Transaction(transaction);
-            if(!map.containsKey(t.name)) {
-                map.put(t.name, new ArrayList<>());
-            }
-            //map.putIfAbsent(t.name, new ArrayList<>());
+        Map<String, List<Transaction>>map = new HashMap<>();
+        for(String transaction : transactions) {
+            Transaction t = new Transaction(transaction); 
+            map.putIfAbsent(t.name,new ArrayList<>());
             map.get(t.name).add(t);
         }
-		// now go through the transaction list again and 
-		// compare each transaction to the respective list for that name
-        for(String transaction : transactions){
-            Transaction t = new Transaction(transaction);
-			// if transaction is not valid, add to final result
-			// (notice we are passing to the function only the list of transactions for that specific name
-			// we only care about transactions for the same name)
-            if(!isValid(t, map.getOrDefault(t.name, new ArrayList<>()))){
+        
+        for(String transaction : transactions) {
+            Transaction t = new Transaction(transaction); 
+            if ( !isValid(t,map.getOrDefault(t.name, new ArrayList<>() ))) {
                 invalid.add(transaction);
+            }  
+                         
+        }  
+        return invalid; 
+
+    }
+    
+    private boolean isValid(Transaction t, List<Transaction>list) {
+        if(t.amount > 1000) {
+            return false; 
+        }
+        for(Transaction k: list) {
+            if( Math.abs(t.minutes - k.minutes) <= 60 && !t.city.equals(k.city)) {
+                return false; 
             }
         }
-        return invalid;
+        return true; 
     }
-    public boolean isValid(Transaction t, List<Transaction> list){
-		// if the transaction is greater than 1000, 
-		/// we hit the first condition in the description so return the transaction as false
-        if(t.amount > 1000){
-			return false;
-		}
-		// go through the list for that name
-		// see if the time of the transaction is less than or equal to 60
-		// and if the transaction is not in the same city
-		// we hit the second condition return the transaction as false
-        for(Transaction ta : list){
-            if(Math.abs(ta.time - t.time) <= 60 && !ta.city.equals(t.city)){
-				return false;
-			}
+    
+     class Transaction{
+        int amount; 
+        int minutes; 
+        String name; 
+        String city; 
+        
+        public Transaction(String transaction) {
+            String [] splitted = transaction.split(","); 
+            this.name = splitted[0];
+            this.minutes = Integer.parseInt(splitted[1]); 
+            this.amount = Integer.parseInt(splitted[2]);
+            this.city = splitted[3]; 
         }
-		// if we go through the entire list and we don't hit any condition, then the transaction is true
-        return true;
+        
     }
 }

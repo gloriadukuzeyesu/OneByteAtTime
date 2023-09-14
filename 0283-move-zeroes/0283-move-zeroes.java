81834e992b2a9efe34007f068dcf8ce120e5e251
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-14 01:51:30 -0600

    Time: 2 ms (62.58%), Space: 45 MB (60.30%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,25 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       //Input: nums = [0,1,0,3,12]
-       // 1 ,0,0,3,12
-       //1,3,12,0,0
-       //1,0,1,3,0
-       if(nums.length<=1 || nums==null){
-           return;
-        }
-        int i = 0;
-        int j = 0;
-        while(j<nums.length){
-           if(nums[j]!=0){
-               int temp = nums[j];
-               nums[j] = nums[i];
-               nums[i] = temp;
-               i++;
-               j++;
-
-            }else {
-               j++;  
+        int i = 0; 
+        for(int j = 0; j < nums.length; j++) {
+            if(nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
             }
         }
+        for(;i < nums.length; i++) {
+            nums[i] =0; 
+        }
+        
     }
 }

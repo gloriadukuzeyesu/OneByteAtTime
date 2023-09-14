1591170230ba08c05fe067deb59d77dae45611eb
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-14 01:48:11 -0600

    Time: 0 ms (100.00%), Space: 42.9 MB (63.62%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -19,15 +19,5 @@ class Solution {
             }
         }
         return head;
-//          ListNode current = head;
-//     while (current != null && current.next != null) {
-//         if (current.next.val == current.val) {
-//             current.next = current.next.next;
-//         } else {
-//             current = current.next;
-//         }
-//     }
-//     return head;
-        
     }
 }

8bf75ab562955465e0b696cf4f8a36c51646e214
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-20 20:08:17 -0600

    Time: 7 ms (42.57%), Space: 46.7 MB (78.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,25 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-       //  strs = ["eat","tea","tan","ate","nat","bat"]
-        // map
-        // Key       value 
-         // aet  ===>   [eat,tea,ate] 
-         // ant  ===>   [tan,nat]
-         // abt  ===>   [bat]
-        
         Map<String, List<String>> map = new HashMap<>(); 
         for(int i = 0; i < strs.length; i++) {
-            String curr = strs[i]; 
-            char[] sortedCurr = curr.toCharArray(); // array
-            Arrays.sort(sortedCurr); 
-            String sortedString = String.valueOf(sortedCurr); 
+            char[] arr = strs[i].toCharArray(); // array
+            Arrays.sort(arr); 
+            String sortedString = String.valueOf(arr); 
             
             if(!map.containsKey(sortedString)) {
-                List<String> val = new ArrayList<>();
-                val.add(curr);
-                map.put(sortedString,val); 
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(sortedString,list); 
             } else {
-                map.get(sortedString).add(curr);
+                map.get(sortedString).add(strs[i]);
                 map.put(sortedString, map.get(sortedString));
             } 
         }

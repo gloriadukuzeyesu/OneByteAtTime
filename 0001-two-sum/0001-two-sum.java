11834524edfda9f0af4f796a4efc5ea61697d08e
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-28 19:33:01 -0600

    Time: 33 ms (54.96%), Space: 43.4 MB (81.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,19 @@
 class Solution {
+    
     public int[] twoSum(int[] nums, int target) {
     
-        Map<Integer, Integer> map = new HashMap<>(); 
-        for(int i = 0; i < nums.length; i++) {
-            int compl = target - nums[i]; // com = 3
-            if(map.containsKey(compl) && map.get(compl) != i) {
-                int[] res = {i, map.get(compl)};
-                return res;
-            } else {
-               map.put(nums[i], i);  
+        for (int i = 0; i < nums.length; i++) {
+              int curr = nums[i]; // 3
+            for(int j = i + 1; j < nums.length; j++) {
+                 // curr + x = target 
+                int x = target - curr; 
+                if(nums[j] == x) {
+                    int[] res = {i,j};
+                    return res;  
+                }
             }
         }
-        return null;        
+        
+        return null; 
     }
 }

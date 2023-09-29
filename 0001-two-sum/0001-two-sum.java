5d8e2bf143d87886e3f8d1686c96719d84861c22
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-09-28 19:34:26 -0600

    Time: 31 ms (55.95%), Space: 43.7 MB (38.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     
     public int[] twoSum(int[] nums, int target) {
-    
         for (int i = 0; i < nums.length; i++) {
               int curr = nums[i]; // 3
             for(int j = i + 1; j < nums.length; j++) {
@@ -13,7 +12,6 @@ class Solution {
                 }
             }
         }
-        
         return null; 
     }
 }

2eaa40b5a95c7a861368dadd0a8f0348f6a0fae5
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-03 12:35:50 -0600

    Time: 26 ms (12.26%), Space: 44 MB (15.10%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.java
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.java
@@ -1,57 +1,41 @@
 class Leaderboard {
-
     Map<Integer, Integer> map; 
+
     public Leaderboard() {
-        map = new HashMap<>();   
+        map = new HashMap<>(); 
+        
     }
     
     public void addScore(int playerId, int score) {
-        if(map.containsKey(playerId)) {
-            map.put(playerId, map.get(playerId) + score); 
-        }else {
-            map.put(playerId, score); 
-        }
+        // check if the player already exists, otherwise udate their score
+        map.put(playerId, map.getOrDefault(playerId, 0) + score); 
     }
     
     public int top(int K) {
-        // sorting 
-        List<Integer> list = new ArrayList<>();
-        for(int key : map.keySet())  {
-            list.add(map.get(key)); // list contains
-        }
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>( (a,b) -> a - b);
+        // max heap. 
+        PriorityQueue<Integer> maxHeap = new PriorityQueue <>( (a,b) -> a - b); 
         for(int score : map.values()) {
-            if(maxHeap.size() < K ) {
-                maxHeap.offer(score); 
-            } else if (score > maxHeap.peek()) {
-                maxHeap.poll(); 
-                maxHeap.offer(score); 
-            }
+            if(maxHeap.size() < K) {
+                // add
+                maxHeap.add(score); 
+            } else {
+                if(score > maxHeap.peek()) {
+                    maxHeap.poll();
+                    maxHeap.add(score); 
+                }
+            }    
         }
-        // int total = 0; 
-        // for(int score : maxHeap) {
-        //     score = score + score; 
-        // }
-// //         for(int i = 0; i < list.size(); i++) {
-// //             if(maxHeap.size() < K) {
-//                 maxHeap.offer(list.get(i)); 
-//             } else if ( maxHeap.peek() > list.get(i) ) {
-//                  maxHeap.poll(); 
-//                  maxHeap.offer(list.get(i)); 
-//             }
-//         }
         
-        // traverse the heap and find sum
-        Iterator <Integer> it = maxHeap.iterator(); 
-        int total = 0; 
-        while(it.hasNext()) {
-            total = total + it.next(); 
+        int sum = 0; 
+        for(int i = 0; i < K ; i++) {
+            sum += maxHeap.poll();
         }
-        return total;    
+        return sum;
+        
     }
     
     public void reset(int playerId) {
-        map.put(playerId, 0);    
+        map.put(playerId, 0); 
     }
 }
 

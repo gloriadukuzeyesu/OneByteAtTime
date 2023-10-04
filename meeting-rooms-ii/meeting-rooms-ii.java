3b144b560c728b52b92b73e0a94706081b0bcf67
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-03 23:31:47 -0600

    Time: 3 ms, Memory: 44.1 MB - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int[]start = new int[intervals.length];
+        int[]end = new int[intervals.length];
+        for(int i = 0; i < intervals.length; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int numRooms = 0;
+        
+        int j = 0;
+        for(int i = 0; i < start.length; i++) {
+            if(start[i] < end[j]) {
+                numRooms ++; 
+            } else {
+                j++;
+            }
+        }
+        return numRooms;
+
+    }
+}

38764ed7acb82c88257c7b94fae6f5a760e30c8b
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-04 00:48:30 -0600

    Time: 3 ms (83.77%), Space: 43.3 MB (31.41%) - LeetHub

---
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.java
@@ -0,0 +1,49 @@
+class Solution {
+       public int calculateSum(int[]arr, int mid) {
+        int sum = 0; 
+        for(int num : arr) {
+            if(num > mid) {
+                sum += mid; 
+            } else{
+                sum += num; 
+            }
+        }
+        return sum;
+    }
+    
+    public int findBestValue(int[] arr, int target) {
+        int l = 0;
+        int r = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            r = Math.max(r, arr[i]);
+        }
+        
+        while(l < r) {
+            // int mid = (l + r) / 2; 
+            int mid = l + ((r - l) / 2);
+            int sum = calculateSum(arr, mid);
+            if(sum == target) {
+                return mid; 
+            }
+      
+            if(sum < target) {
+                l = mid + 1; 
+            } else{
+                r = mid;
+            }
+            
+ 
+        }
+        
+        int first = calculateSum(arr,l);
+        int second = calculateSum(arr,l-1);
+        
+        if(Math.abs(first-target) < Math.abs(second-target)) {
+            return l;
+        }
+        
+        return l - 1;
+
+    }
+ 
+}

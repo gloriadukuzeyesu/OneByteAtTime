296ffa77ebce2678e6b3bd6b781fec8d21d1a9d3
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-05 15:08:07 -0600

    Time: 2 ms (76.60%), Space: 42.8 MB (86.93%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.java
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.java
@@ -1,36 +1,48 @@
 class Solution {
     public long maxStrength(int[] nums) {
+        long posPr = 1; 
+        long negPro = 1; 
         
-        long posProd = 1;
-        long negProd = 1;
         int maxNeg = Integer.MIN_VALUE;
-        int negCount = 0;
-        boolean hasPos = false;
-        boolean hasZero = false;
-
-        for (int num : nums) {
-            if (num > 0) {
-                posProd *= num;
-                hasPos = true;
+        int NegCount = 0; 
+        
+        boolean hasPos = false; 
+        boolean hasZero = false; 
+        
+        for(int num : nums) {
+            if(num > 0) {
+                posPr *= num;
+                hasPos = true; 
             } else if (num < 0) {
-                negProd *= num;
-                maxNeg = Math.max(maxNeg, num);
-                negCount++;
+                // negative 
+                negPro *= num; 
+                maxNeg = Math.max(maxNeg, num); 
+                NegCount++;  
             } else {
                 hasZero = true;
             }
         }
-
-        if (negCount == 0 && !hasPos) return 0;
-        if (negCount % 2 == 0) return posProd * negProd ;
-        if (negCount >= 2) return (negProd / maxNeg) * posProd;
-        if (hasPos) return posProd;
-        if (hasZero) return 0;
-
+        
+        // if we no negative number and no positive
+        if(NegCount == 0 && !hasPos) {
+            return 0;
+        }
+        
+        // if there is even neg number
+        if(NegCount % 2 == 0) {
+            return posPr * negPro; 
+        }
+        
+        // in case there is an odd number of negatives, remove the max one to maximize the product
+        if(NegCount >= 2){
+           return (negPro / maxNeg) * posPr;
+        }
+        if(hasPos) {
+            return posPr;
+        }
+        if(hasZero) {
+            return 0;
+        }
         return maxNeg;
-        
-
- 
-        
     }
 }

7b2d32c3eb627ac88498c609ec296b9d9afe5873
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-07 21:50:15 -0600

    Time: 1 ms (97.71%), Space: 39.4 MB (71.16%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,15 +1,41 @@
+// class Solution {
+//     public int reverse(int x) {
+//         //123
+//         if(x == 0) return 0;
+//         if(x > Integer.MAX_VALUE && x < Integer.MIN_VALUE) return 0; 
+            
+//         boolean isFalse = false;
+        
+//         if(x < 0) {
+//             isFalse = true; 
+//             x = -1 * x;
+//         }
+//         int res = 0;
+//         while ( x != 0 ) {
+//             res =  res * 10 + (x % 10);
+//             x = x / 10;   
+//         }
+        
+        
+//         if(isFalse) {
+//             res = -res;
+
+//         }
+        
+//         return res; 
+//     }
+// }
+
 class Solution {
     public int reverse(int x) {
-        int result = 0; 
-        int xremain = Math.abs(x); 
-        while(xremain != 0) {
-            int pop = xremain % 10; 
-            xremain /= 10; 
-            if (result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            if (result < Integer.MIN_VALUE/10 || (result == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            result = result * 10 + pop; 
-            
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
         }
-        return x < 0 ? -result : result; 
+        return rev;
     }
 }

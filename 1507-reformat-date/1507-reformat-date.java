a9aa6857882f181f762a037f6f4e543ad6ea4d3b
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-22 10:50:54 -0600

    Time: 1 ms (94.35%), Space: 40.9 MB (46.30%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.java
+++ 1507-reformat-date/1507-reformat-date.java
@@ -1,49 +1,45 @@
 class Solution {
     public String reformatDate(String date) {
         String[] dateSplited = date.split(" ");
-        Map<String, Integer> months = new HashMap<>(); 
-        months.put("Jan", 1);
-        months.put("Feb", 2);
-        months.put("Mar", 3);
-        months.put("Apr", 4);
-        months.put("May", 5);
-        months.put("Jun", 6);
-        months.put("Jul", 7);
-        months.put("Aug", 8);
-        months.put("Sep", 9);
-        months.put("Oct", 10);
-        months.put("Nov", 11);
-        months.put("Dec", 12);
+        Map<String, String> months = new HashMap<>(); 
+        months.put("Jan", "01");
+        months.put("Feb", "02");
+        months.put("Mar", "03");
+        months.put("Apr", "04");
+        months.put("May", "05");
+        months.put("Jun", "06");
+        months.put("Jul", "07");
+        months.put("Aug", "08");
+        months.put("Sep", "09");
+        months.put("Oct", "10");
+        months.put("Nov", "11");
+        months.put("Dec", "12");
         
         StringBuilder sb = new StringBuilder(); 
         
-        String dayOriginal = dateSplited[0];
-        String day = "";
-        for(int i = 0; i < dayOriginal.length(); i++) {
-            if(Character.isDigit(dayOriginal.charAt(i))) {
-                day += dayOriginal.charAt(i);
-            } else{
-                break; 
-            }
-        }
-        if(day.length() == 1) {
-            day = "0" + day; 
-        }
+       //sb.append(ss[0].length() == 3 ? ("0" + ss[0].substring(0, 1)) : ss[0].substring(0, 2));
+        
+
+        String dayOg = dateSplited[0];
+        String day = dayOg.length() == 3 ? ("0" + dayOg.substring(0, 1)) : dayOg.substring(0, 2); 
+            
+//        = "";
+//         for(int i = 0; i < dayOriginal.length(); i++) {
+//             if(Character.isDigit(dayOriginal.charAt(i))) {
+//                 day += dayOriginal.charAt(i);
+//             } else{
+//                 break; 
+//             }
+//         }
+//         if(day.length() == 1) {
+//             day = "0" + day; 
+//         }
         
        String monthOriginal = dateSplited[1];
-       int monthInt = months.get(monthOriginal); 
-        String month = "";
-        if(monthInt < 10) {
-            month = String.valueOf("0" + monthInt); 
-        } else {
-            month = String.valueOf(monthInt);
-        }
-        //YYYY-MM-DD
-        
+       String month = months.get(monthOriginal); 
+    
         String year = dateSplited[2];
-        
-        sb.append(year);
-        sb.append("-").append(month).append("-").append(day);
+        sb.append(year).append("-").append(month).append("-").append(day);
         return sb.toString();
     }
 }

b93900e119ea6a71e347605cd8f87c1ea9efb6c4
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-22 10:22:59 -0600

    Time: 2 ms (94.67%), Space: 40.4 MB (74.76%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,30 +1,28 @@
 class Solution {
+    
     public int countSubstrings(String s) {
         int count = 0; 
+        if(s.length() < 0) {
+            return count; 
+        }
         for(int i = 0; i < s.length(); i++) {
-            for(int j = i; j < s.length(); j++) {
-                if(subString(s, i, j)) {
-                    count++; 
-                }
-            }
-            
+            count += subString(s,i,i); // odd 
+            count += subString(s,i,i+1); // even
         }
-        return count; 
-        
+        return count;
     }
-    
-    public boolean subString(String s, int i , int j ) {
-        if(i < 0 || j > s.length() - 1) {
-            return false; 
-        }
-        while( i <= j) {
-            if(s.charAt(i) != s.charAt(j)) {
-                return false; 
-            } else {
-                i++;
-                j--; 
-            }
-        }
-        return true; 
+    public int subString( String s, int i, int j) {
+        int count = 0;
+         while( i >= 0 && j < s.length()) {
+             if(s.charAt(i) != s.charAt(j)){
+                 break; 
+             } else {
+                 i--;
+                 j++; 
+                 count++;
+             }
+         }
+        return count; 
+       
     }
 }

fdaa8212c8cc3c13acd24308b4f089c380468141
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-10-26 18:28:42 -0600

    Time: 3 ms (52.41%), Space: 44.4 MB (50.94%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,31 +1,5 @@
 class Solution {
-    // 1. understand
-    // 2. brainstorm
-    // 3. implement
-    
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-       // K - V
-        //. 1 - 6
-        //  2 - 3
-        //  4 - 6
-        //. 3 - 2
-        //building list out map
-        
-        // [1,2,3,4] / nlogn 
-        
-        // [[1,6], [2,3],[3,2],[4,6]]
-        
-        //index_checker = 0  
-                
-        // nums1 = [ [1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]]
-                     //nums1[i][0]                                   j 
-        //  
-        //sub:List
-        
-       // [[2,4],[4,6],[8,5], [9,10]], nums2 = [[1,3],[4,3]]
-        
-       // [1,3],[2,4], [4,9]
-        
         List<List<Integer>> list = new ArrayList<>(); 
         int num1Size = nums1.length; 
         int num2Size = nums2.length; 

b8f4ff0c6fd00bc4184258c0e9cbee199d5f578d
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-04 20:17:29 -0500

    Time: 2 ms (100.00%), Space: 43.7 MB (49.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,39 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-
-        Map<String, Integer> map = new HashMap<>(); 
-        map.put("M" , 1000);
-        map.put("D" , 500);  
-        map.put("C" , 100);
-        map.put("L" , 50);
-        map.put("X" , 10);  
-        map.put("V" , 5);
-        map.put("I" , 1);
-
-        int N = s.length();
-        int i=0;
-        int total =0;  
-        while(i < N){
-            String currentSymbol = s.substring(i,i+1);
-            int currentValue = map.get(currentSymbol);
-            int nextValue=0;
-
-            if (i+1 < N){
-                String nextSymbol = s.substring(i+1, i+2);
-                nextValue = map.get(nextSymbol);
+        
+        int ans = 0, num = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            switch(s.charAt(i)) {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
             }
-            if(currentValue < nextValue){
-                total = total + (nextValue - currentValue);
-                i+=2;
-            } else if(currentValue == nextValue){
-                total = total + currentValue+nextValue;
-                i+=2;
-            }
-            else{
-                total += currentValue;
-                i++;
-            }
+            if (4 * num < ans) ans -= num;
+            else ans += num;
         }
-        return total;  
+        return ans;
     }
 }

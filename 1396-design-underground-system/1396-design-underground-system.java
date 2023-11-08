a2d40d051a65db68a74d6a6f92356637ba0c3fad
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-08 17:58:50 -0600

    Time: 91 ms (51.67%), Space: 54.5 MB (46.30%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -0,0 +1,52 @@
+class UndergroundSystem {
+    Map<Integer, ArrivalInfo> checkIntrack; // [id, [id, stationName, t]]
+    Map<String, double[]> totals; // 
+
+    public UndergroundSystem() {
+        checkIntrack = new HashMap<>(); 
+        totals = new HashMap<>();  
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+        //Pair<String, Integer> pair = new Pair<String, Integer>(stationName, t); 
+        checkIntrack.put(id, new ArrivalInfo(id,stationName, t));  
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+        //checkOuttrack.put(id, new Pair<String, Integer>(stationName, t)); 
+        ArrivalInfo info = checkIntrack.get(id); 
+        String src = info.stationName;
+        String key = src + "_" + stationName;
+        int tripTime = t - info.time; 
+        double[] pair = totals.getOrDefault(key, new double[2]); 
+        pair[0] += tripTime;
+        pair[1]++;
+        totals.put(key, pair); 
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        String key = startStation + "_" + endStation; 
+        double[] pair = totals.get(key); 
+        return pair[0] / pair [1]; 
+    }
+}
+
+// Custom object 
+public class ArrivalInfo{
+    int id; 
+    String stationName; 
+    int time; 
+    public ArrivalInfo(int id, String stationName, int time) {
+        this.id = id; 
+        this.stationName = stationName;
+        this.time = time; 
+    }
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */

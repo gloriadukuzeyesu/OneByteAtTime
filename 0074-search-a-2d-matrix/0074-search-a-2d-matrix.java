b1edd1465258c7578158e1ca787db570ba2965d8
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-10 22:14:17 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (55.75%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -8,19 +8,20 @@ class Solution {
         // Step 1: Binary search to find the row where the target might be located.
         int left = 0; 
         int right = rows - 1; 
-        
         while( left < right) {
             int mid = left + (right - left) / 2; 
             int midElement = matrix[mid][cols - 1]; 
+            if(midElement == target) return true; 
+            
             if(midElement < target) {
                 left = mid + 1; 
             } else {
                 right = mid; 
             }
-        }
-            
+        }  
+        // the right holds the index of the row where the target might located. 
+        // step 2. Binary seach the row to find target 
         return searchRow(matrix[right], target); 
-
     }
     private boolean searchRow (int [] row, int target) {
         int l = 0; 

009e8d1b5eb9229d89844f6b2155b7d435f88806
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-12 23:29:05 -0700

    Time: 1 ms (99.23%), Space: 42.5 MB (93.07%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,37 +13,74 @@
  *     }
  * }
  */
-class Solution {
-    public int widthOfBinaryTree(TreeNode root) {        
-        int maxCount = 0; 
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>(); 
-        queue.add(new Pair<>(root,0)); 
-        maxCount = queue.size(); 
-        
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            int indexF = queue.peek().getValue();
-            
-            for(int i = 0; i < size; i++) {
-                Pair<TreeNode, Integer> curr = queue.poll();
-                TreeNode currNode = curr.getKey();
-                int currIndex = curr.getValue();
-                
-                if(currNode.left != null) {
-                     queue.add(new Pair<>(currNode.left, currIndex * 2));
-                }
 
-                if(currNode.right != null) {
-                    queue.add(new Pair<>(currNode.right, currIndex * 2 + 1));
-                }
-                
-                int currWidth = currIndex - indexF + 1; 
-                if(maxCount <  currWidth) {
-                    maxCount = currWidth;
-                }
-            }
- 
+class Combination {
+    TreeNode node; 
+    int index; 
+    public Combination(TreeNode node, int index) {
+        this.node = node; 
+        this.index = index; 
+    }
+    
+}
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null) {
+            return 0;
         }
-        return maxCount;
+        
+        int maxWidth = 0;
+        LinkedList<Combination> queue = new LinkedList<>();
+
+        queue.addLast(new Combination(root, 0));
+
+        while (queue.size() > 0) {
+            Combination head = queue.getFirst();
+
+            // iterate through the current level
+            int currLevelSize = queue.size();
+            Combination elem = null;
+            for (int i = 0; i < currLevelSize; ++i) {
+                elem = queue.removeFirst();
+                TreeNode node = elem.node;
+                if (node.left != null)
+                    queue.addLast(new Combination(node.left, 2 * elem.index));
+                if (node.right != null)
+                    queue.addLast(new Combination(node.right, 2 * elem.index + 1));
+            }
+
+            // calculate the length of the current level,
+            // by comparing the first and last col_index.
+            maxWidth = Math.max(maxWidth, elem.index - head.index + 1);
+        }
+
+        return maxWidth;
+//         int maxWidth = 0; 
+//         Queue<Combination> queue = new LinkedList<>(); 
+//         queue.add(new Combination(root, 0)); 
+//         while(!queue.isEmpty()) {
+//               int size = queue.size(); 
+//             // head
+//             int start = queue.poll().index; 
+//             int end = 0; 
+//             for(int i = 0; i < size; i++) {
+//                 Combination head = queue.poll();
+//                 if(head != null) {
+//                      end = head.index;
+//                 }
+//                 if(head.node.left != null) {
+//                     queue.offer(new Combination(head.node.left, head.index * 2));
+//                 }
+//                 if(head.node.right != null) {
+//                    queue.offer(new Combination(head.node.right, (head.index * 2) + 1)); 
+//                 }
+//             }
+//             int width = end - start + 1; 
+//             maxWidth = Math.max(maxWidth, width); 
+            
+//         }
+//         return maxWidth; 
     }
 }
+

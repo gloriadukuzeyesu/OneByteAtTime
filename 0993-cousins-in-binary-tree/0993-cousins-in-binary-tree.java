278e7e5ccb47e811efd3a36d18b9f06451e1c897
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-13 16:28:49 -0700

    Time: 0 ms (100.00%), Space: 39.9 MB (75.78%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -14,36 +14,42 @@
  * }
  */
 class Solution {
-    int recordedDepth = -1; 
-    boolean isCousin = false; 
-    
     public boolean isCousins(TreeNode root, int x, int y) {
-        dfs(root, 0, x, y);
-        return isCousin; 
-    }
-    public boolean dfs(TreeNode node, int depth, int x, int y) {
-        if(node == null) {
-            return false; 
-        }
-        // Don't go beyond the depth restricted by the first node found.
-        if(recordedDepth != -1 && depth > recordedDepth) {
-            return false; 
-        }
-        
-        if(node.val == x || node.val == y) {
-            // record this depth 
-            if(recordedDepth == -1) {
-                recordedDepth = depth; 
+        if(root == null) return false; 
+        Queue<TreeNode> queue = new LinkedList<>(); 
+        queue.add(root); 
+        int depthX = -1; 
+        int depthY = -2; 
+        int level = 0; 
+        while(!queue.isEmpty())  {
+            int size = queue.size(); 
+            for(int i = 0; i < size; i++) {
+                TreeNode currNode = queue.poll(); 
+                if(currNode.left != null && currNode.right != null && currNode.left.val == x && currNode.right.val == y) {
+                    return false; // same parent. 
+                }
+                if(currNode.left != null && currNode.right != null && currNode.left.val == y && currNode.right.val == x) {
+                    return false; // same parent. 
+                }
+                if(currNode.val == x) {
+                    depthX = level; 
+                }
+                if(currNode.val == y) {
+                    depthY= level; 
+                }
+                
+                // add to queu
+                if(currNode.left != null) {
+                    queue.add(currNode.left);
+                }
+                
+                if(currNode.right != null) {
+                    queue.add(currNode.right); 
+                }
+                
             }
-            return recordedDepth == depth; 
-        }  
-        
-        boolean left = dfs(node.left, depth + 1, x, y); 
-        boolean right = dfs(node.right, depth + 1, x, y);
-        
-        if(left && right && recordedDepth != depth + 1) {
-            isCousin = true; 
+            level++; 
         }
-        return left || right; 
+        return depthX == depthY; 
     }
 }

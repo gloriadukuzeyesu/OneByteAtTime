f18539ebeae5c73005218415c83c5e1e259bc0b7
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-14 13:36:54 -0700

    Time: 6 ms (10.29%), Space: 44 MB (40.48%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -13,13 +13,18 @@ class Solution {
         if(root == null) {
             return null; 
         }
-        if(root == p || root == q) {
-            return root; 
-        }
+        // Since it is a bst. 
+        // find the side where both q and p resides
+        // if they are on both sides of the tree, the current root is the split point.
+        // return that.
+        // time complexity : O(n)
+        // space complexity : O(n) space used by the recursion stack 
+        
         if(p.val < root.val && q.val < root.val) {
             // left search 
             return lowestCommonAncestor(root.left, p, q); 
         } else if(p.val > root.val && q.val > root.val) {
+            // right seach 
             return lowestCommonAncestor(root.right, p, q); 
         } else {
             return root; // split point. 

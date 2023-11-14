c8f0839ddcc04ff2928b54e16a69e651990a0fa1
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-11-14 13:07:37 -0700

    Time: 0 ms (100.00%), Space: 40.4 MB (10.13%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,14 +15,14 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
-            return true; 
-        }
-        if(p == null) return false;
-        if(q == null) return false; 
-        if(p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.left ) && isSameTree(p.right, q.right);
+        return dfs(p,q); 
+    }
+    public boolean dfs(TreeNode p, TreeNode q) {
+        if(p == null && q== null) return true;
+        if((p == null && q != null) || (p != null && q == null)) return false; 
+        if(p.val != q.val) return false; 
+        boolean checkLeftLeg = dfs(p.left, q.left); 
+        boolean checkRightLeg = dfs(p.right, q.right); 
+        return checkLeftLeg && checkRightLeg; 
     }
 }

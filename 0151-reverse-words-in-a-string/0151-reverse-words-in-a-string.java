d8739fabc1c97f1e549b91dd07594226a7f5e061
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2023-12-16 16:15:42 -0700

    Time: 8 ms (58.61%), Space: 42.3 MB (69.97%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reverseWords(String s) {
+      s = s.trim(); 
+      List<String> list = Arrays.asList(s.split("\\s+"));
+      Collections.reverse(list);
+      return String.join(" ", list); 
+        
+    }
+}

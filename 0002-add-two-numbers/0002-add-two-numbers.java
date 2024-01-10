91b1b08f2f44cf214a475beabd19ccbd60a99567
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-09 20:37:07 -0700

    Time: 1 ms (100.00%), Space: 44.7 MB (10.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,20 +10,49 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0); 
-        ListNode currentNode = dummy; 
-        int carry = 0; 
-        while(l1 != null ||  l2 != null || carry != 0) {
-            int x = l1 != null ? l1.val : 0; 
-            int y = l2 != null ? l2.val : 0;
-            int sum = carry + x + y; 
-            currentNode.next = new ListNode(sum % 10); 
-            carry = sum / 10; 
-            currentNode = currentNode.next; 
-            if(l1 != null ) l1 = l1.next; 
-            if(l2 != null ) l2 = l2.next;  
-        }
-        
-        return dummy.next; 
+       ListNode p1 = l1; 
+       ListNode p2 = l2;
+       ListNode dummyHead = new ListNode(0);
+       ListNode current = dummyHead;
+       int carry = 0; 
+      while(p1 != null && p2 != null) {
+          int sum = p1.val + p2.val; //7
+          sum += carry; //8
+          if (sum > 9) {
+              int rem = sum % 10; // 10
+              current.next = new ListNode(rem);
+          } else{
+              current.next = new ListNode(sum);
+          }
+          current = current.next;  
+          carry = sum / 10;   // 0
+          p1 = p1.next;
+          p2 = p2.next;
+      }
+      
+      while(p1 != null || p2 != null) {
+          if(p1 != null) {
+              int total = p1.val + carry;
+              int rem = total % 10;
+              current.next = new ListNode(rem);
+              current = current.next; 
+              carry = total / 10; 
+              p1 = p1.next;
+          } else{
+              // p1 == null
+              int total = p2.val + carry;
+              int rem = total % 10; 
+              current.next = new ListNode(rem);
+              current = current.next; 
+              carry = total / 10; 
+              p2 = p2.next;
+          }
+      }
+
+      if(carry != 0) {
+         current.next = new ListNode(carry);
+          
+      }
+       return dummyHead.next; 
     }
 }

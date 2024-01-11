89176565ae631fa74f37e54ffb1bf3a8a05d0c60
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-10 23:53:23 -0700

    Time: 0 ms (100.00%), Space: 41.4 MB (30.82%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -5,44 +5,35 @@ class Node {
     public Node prev;
     public Node next;
     public Node child;
-    
-    public Node() {}
-
-    public Node(int _val,Node _prev,Node _next,Node _child) {
-        val = _val;
-        prev = _prev;
-        next = _next;
-        child = _child;
-    }
-    
 };
 */
 
 class Solution {
     public Node flatten(Node head) {
-        if(head == null) {
-            return head; 
-        }
-        
-        Node p = new Node (0, null, head, null); 
-        dfs(p, head); 
-        p.next.prev = null; 
-        return p.next; 
-        
+        dfs(head);
+        return head;
     }
-      /* return the tail of the flatten list */
-
-    public Node dfs(Node prev, Node curr) {
-        if(curr == null) {
-            return prev; 
-        } 
-        curr.prev = prev; 
-        prev.next = curr; 
-        
-        Node tempNext = curr.next; 
-        Node tail = dfs(curr, curr.child); 
-        curr.child = null; 
-        
-        return dfs(tail, tempNext); 
+    public Node dfs(Node curr) {
+        Node prev = curr; 
+        while(curr != null) {
+            prev = curr;
+            if(curr.child != null) {
+                Node tail = dfs(curr.child);
+                tail.next = curr.next;
+                if(curr.next != null) {
+                    curr.next.prev = tail;
+                }
+                curr.next = curr.child; 
+                curr.child.prev = curr;
+                curr.child = null;
+                
+                curr = tail.next;
+                prev = tail;
+                
+            } else {
+                curr = curr.next;
+            }
+        }
+        return prev;
     }
 }

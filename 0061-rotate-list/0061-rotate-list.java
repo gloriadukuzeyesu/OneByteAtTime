9ae041c31a7aa4687062430c7196e8c9f54d2511
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-17 23:59:52 -0700

    Time: 0 ms (100.00%), Space: 42.8 MB (11.87%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,37 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+        
+//     }
+// }
+
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null) {
-            return null; 
+        if(head == null || head.next == null || k == 0) {
+            return head; 
         }
-
-        if(head.next == null) {
-            return head;  
-        }
-        int len = 1; 
+        // length
+        int n = 1; 
         ListNode oldTail = head; 
         while(oldTail.next != null) {
+            n++;  
             oldTail = oldTail.next; 
-            len++; 
+        }
+
+        k = k % n; /// 2 % 4
+        
+        if(k == 0) {
+            return head; 
+        }
+
+        ListNode newtail = head; 
+        int p = n - k; 
+        while( p > 1) {
+            newtail = newtail.next;
+            p--; 
         }
         
-        // conect the old tail to the head
-        oldTail.next = head; 
+        ListNode newHead = newtail.next; 
+        newtail.next = null; // cut connection 
         
-        k = k % len; // in case k is greater than len 
-        
-        // find the new tail
-        int n = len - k - 1; // newtail 
-        ListNode newTail = head; 
-        
-        for(int i = 0; i < n; i++) {
-            newTail = newTail.next; 
-        }
-        ListNode newHead = newTail.next; 
-        newTail.next = null;
+        oldTail.next = head;         
         return newHead; 
-        
     }
 }

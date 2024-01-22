bf04212f5a6abcece305627160514a420db9549b
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-22 10:32:26 -0700

    Time: 2 ms (71.09%), Space: 41.4 MB (41.90%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,17 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character, Character> map = new HashMap<>(); 
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
+        Map<Character, Character> brackets = new HashMap<>(); 
+        brackets.put('(', ')'); 
+        brackets.put('{', '}'); 
+        brackets.put('[', ']');
+        
+        // stack
         Stack<Character> stack = new Stack<>(); 
-        for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty() || map.get( stack.pop() ) != c) {
-                    return false;
+        for(char c : s.toCharArray()) {
+            if(!brackets.containsKey(c)) {
+                if(!stack.isEmpty()) {
+                    char open_bracket = stack.peek(); 
+                    char closing_bracket = brackets.get(open_bracket); 
+                    if(closing_bracket != c) {
+                        return false; 
+                    } else{
+                        stack.pop(); 
+                    }
+                } else {
+                    return false; 
                 }
+                
+            } else {
+                stack.push(c); 
             }
         }
         return stack.isEmpty(); 

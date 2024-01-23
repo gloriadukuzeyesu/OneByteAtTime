e031c595a23e0131d5f72dc268832378051d04fa
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-23 13:19:48 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (47.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null) return -1; 
-        int l = 0, r = nums.length - 1; 
-        while( l <= r) {
-            int mid = l + (r - l) / 2; 
-            System.out.println(mid); 
-            if(nums[mid] == target) {
-                return mid; 
-            } else if(nums[mid] >= nums[l]){ // 3 > 3 
-                if(nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1 ; 
+        int left = 0; 
+        int right = nums.length - 1; 
+        
+        while(left <= right ) {
+            int mid = left + (right - left) / 2; 
+            if (nums[mid] == target) return mid; 
+            
+            if(nums[mid] >= nums[left]) { // middle num belongs to left sorted portion of the array 
+                if(target > nums[mid] || target < nums[left]) {
+                    // search on the right
+                    left = mid + 1; 
                 } else {
-                    l = mid + 1; 
+                    right = mid - 1; 
                 }
-
+                
             } else {
-                if(nums[mid] < target && target <= nums[r]) { // 3 < 1 && 1 <= 1 
-                    l = mid + 1; 
+                // nums[mid] belongs to the right side. 
+                if(target < nums[mid] || target > nums[right]) {
+                    right = mid - 1; 
                 } else {
-                    r = mid - 1; 
+                    left = mid + 1; 
                 }
             }
         }
-        return -1;
-
+        return -1; 
     }
 }

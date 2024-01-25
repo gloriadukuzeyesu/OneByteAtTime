7905ee6fbdfe126cd6d27e3dd2a4ae2379b0228e
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-25 14:49:14 -0700

    Time: 5 ms (12.11%), Space: 47.9 MB (52.21%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,40 +10,44 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-       
-        if(head == null && head.next == null) {
+        if(head == null || head.next == null) {
             return; 
         }
+        
         ListNode slow = head; 
         ListNode fast = head; 
         
         while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+            slow = slow.next; 
+            fast = fast.next.next; 
+        }
+              
+        // reverse it
+        ListNode secondHalf = reverse(slow); 
+        System.out.println("secondHalf: " + secondHalf.val); 
+        // merge 
+        ListNode firstHalf = head; 
+        while(secondHalf.next!= null) {
+           ListNode temp1 = firstHalf.next; 
+           ListNode temp2 = secondHalf.next; 
+           
+           firstHalf.next = secondHalf; 
+           secondHalf.next = temp1;
+            
+           firstHalf = temp1;    
+           secondHalf = temp2; 
         }
         
-        // reverse second half
-        ListNode curr = slow; 
-        ListNode prev = null;
-        while (curr != null) {
+    }
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null; 
+        ListNode curr = head;
+        while(curr != null) {
             ListNode temp = curr.next; 
-            curr.next = prev;
-            prev = curr;
+            curr.next = prev; 
+            prev = curr; 
             curr = temp; 
         }
-
-        // merge 
-        ListNode first = head, second = prev;
-        while(second.next != null) {
-            ListNode temp = first.next; 
-            first.next = second; 
-            first = temp;
-            temp = second.next; 
-            second.next = first; 
-            second = temp; 
-            
-        }
-        //head.next = null; 
-            
+        return prev; 
     }
 }

52b32d6f36e376c66b79c0f46b42c54a401f8ecb
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-25 13:59:02 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (33.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,26 +10,34 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode res = new ListNode(0); 
-        ListNode dummyhead = res; 
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) {
-                res.next = list1;  
-                list1 = list1.next; 
+        ListNode dummy = new ListNode(0); 
+        ListNode curr = dummy;
+        ListNode list1_pointer = list1; 
+        ListNode list2_pointer = list2; 
+        while(list1_pointer != null && list2_pointer != null) {
+            if(list1_pointer.val <= list2_pointer.val) {
+                curr.next = list1_pointer; 
+                list1_pointer = list1_pointer.next; 
             } else {
-                res.next = list2;
-                list2 = list2.next; 
+                curr.next = list2_pointer; 
+                list2_pointer = list2_pointer.next;  
             }
-            res = res.next; 
+            curr = curr.next; 
+        }
+        // in case l1 ends before l2
+        while(list2_pointer != null) {
+            curr.next = list2_pointer;
+            list2_pointer = list2_pointer.next; 
+            curr  = curr.next; 
         }
         
-        if(list1 != null) {
-            res.next = list1; 
+        // case where l2 ends before l1
+        while(list1_pointer != null) {
+            curr.next = list1_pointer; 
+            curr = curr.next; 
+            list1_pointer = list1_pointer.next;    
         }
-        if(list2 != null) {
-            res.next = list2; 
-        }
-        return dummyhead.next; 
         
+        return dummy.next; 
     }
 }

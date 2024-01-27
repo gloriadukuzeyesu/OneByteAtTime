ccd4cbd0bbabc18e5f9cdbe5d9398c7a8763605d
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-27 14:11:10 -0700

    Time: 0 ms (100.00%), Space: 41 MB (39.22%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,14 +15,16 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return dfs(p,q); 
-    }
-    public boolean dfs(TreeNode p, TreeNode q) {
-        if(p == null && q== null) return true;
-        if((p == null && q != null) || (p != null && q == null)) return false; 
+        if(p == null && q == null) return true; 
+        if(p == null && q != null) return false; 
+        if (p != null && q == null) return false; 
+        
         if(p.val != q.val) return false; 
-        boolean checkLeftLeg = dfs(p.left, q.left); 
-        boolean checkRightLeg = dfs(p.right, q.right); 
-        return checkLeftLeg && checkRightLeg; 
+        
+       
+        boolean leftCheck = isSameTree(p.left, q.left); 
+        boolean rightCheck = isSameTree(p.right, q.right); 
+        return leftCheck && rightCheck; 
+        
     }
 }

c5f4540058566202264457e553cfbe5c76fe0ed2
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-26 18:20:53 -0700

    Time: 4 ms (69.67%), Space: 44.7 MB (20.07%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,38 +10,25 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null; 
-        if(lists.length == 1) return lists[0]; 
-        ListNode head = mergeTwoLists(lists[0], lists[1]); 
-        for(int i = 2; i < lists.length; i++) {
-            head = mergeTwoLists(head, lists[i]);
-        }
-        return head; 
+        // create a min heap
+        // put a every list in a min heap
+        // create a ListNode from that min heap
         
-        
-    }
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode p1 = list1; 
-        ListNode p2 = list2; 
-        ListNode dummy = new ListNode(0); 
-        ListNode mergedList = dummy; 
-        while(p1 != null && p2 != null) {
-            if(p1.val <= p2.val) {
-                mergedList.next = p1; 
-                p1 = p1.next; 
-            } else {
-                mergedList.next = p2; 
-                p2 = p2.next; 
+        // Space complexity is O(n)
+        // time complexity is O(nlog K)
+        // where K is the number of linked lists 
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); 
+        for(ListNode head : lists) {
+            while(head != null) {
+                minHeap.add(head.val); 
+                head = head.next; 
             }
-            mergedList = mergedList.next; 
         }
-        
-        if(p1 != null) {
-            mergedList.next = p1; 
-        }
-        
-        if(p2 != null) {
-            mergedList.next = p2; 
+        ListNode dummy = new ListNode(0); 
+        ListNode head = dummy;
+        while(!minHeap.isEmpty()) {
+            head.next = new ListNode(minHeap.remove()); 
+            head = head.next; 
         }
         
         return dummy.next; 

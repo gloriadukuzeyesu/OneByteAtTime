c6de63883e210bdfe61dfbef8371314f989876b4
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-28 16:14:20 -0700

    Time: 2 ms (83.75%), Space: 44.9 MB (11.61%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> answer = new ArrayList<>(); 
-        List<Integer> combination = new ArrayList<>(); 
-        backtrack(candidates, 0, answer, combination, target); 
-        return answer; 
+        List<Integer> combination = new ArrayList<>();
+        List<List<Integer>> results = new ArrayList<>(); 
+        backTracking(candidates, 0, combination, results, target); 
+        return results; 
     }
-    public void backtrack(int[] candidates, int index, List<List<Integer>> answer, List<Integer> curr_combination, int target) {
+    public void backTracking(int[] candidates,
+                             int index,  
+                             List<Integer> combination,  
+                             List<List<Integer>> results, 
+                             int target) {
         if(target == 0) {
-            answer.add(new ArrayList<>(curr_combination)); 
-            return; 
+            results.add(new ArrayList<>(combination)); 
         }
         if(target < 0) {
             return; 
         }
         
         if(index >= candidates.length) {
-            return; 
+            return;
         }
-  
+        
         for(int i = index; i < candidates.length; i++) {
-            curr_combination.add(candidates[i]); 
-            backtrack(candidates, i, answer, curr_combination, target - candidates[i]);
-            curr_combination.removeLast(); 
+            combination.add(candidates[i]); 
+            backTracking(candidates, i, combination, results, target -  candidates[i]); 
+            combination.remove(combination.size() - 1); 
         }
     }
+
 }

703d8d855dd2b01b96803b737c6a5448937900e7
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-01-28 16:40:25 -0700

    Time: 130 ms (85.92%), Space: 41.5 MB (53.97%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,38 +1,39 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        // find the first char
-        // perfom dfs in four directions to find next character
         int rows = board.length; 
-        int cols = board[0].length;  
-        for(int i = 0; i < rows; i++) { 
-            for(int j = 0; j < cols; j++) {
-                if(board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0)) {
-                   return true; 
+        int cols = board[0].length; 
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if( board[r][c] == word.charAt(0) && dfs(board,0, word, r, c) ){
+                    return true;
                 }
             }
         }
         return false; 
     }
-    public boolean dfs(char[][] grid, int row, int col, String word, int index) {
-     
-        // check boundaries
-        if(row >= grid.length || row < 0 || col >= grid[0].length || col < 0 || grid[row][col] != word.charAt(index)) {
+    public boolean dfs (char[][] board, int index, String word, int r, int c) {
+        
+        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
             return false; 
         }
         
         if(index == word.length() - 1) {
-            return true; // word is found. 
+            System.out.println("1"); 
+            return true; // word has been found 
         }
-        grid[row][col] = '#'; // mark it 
-   
-        if( dfs(grid, row + 1, col, word, index + 1) || 
-            dfs(grid, row + 1, col, word, index + 1) || 
-            dfs(grid, row - 1, col, word, index + 1) || 
-            dfs(grid, row, col + 1, word, index + 1) ||
-            dfs(grid, row, col - 1, word, index + 1)) {
-             return true; 
+        
+        board[r][c] = '#'; 
+    
+        if(dfs(board, index + 1, word, r + 1, c) || 
+        dfs(board, index + 1, word, r - 1, c) ||
+        dfs(board, index + 1, word, r, c + 1) ||
+        dfs(board, index + 1, word, r, c - 1)) {
+            System.out.println("2"); 
+            return true; 
         }
-        grid[row][col] = word.charAt(index); 
-      return false; 
+        
+        board[r][c] = word.charAt(index); 
+        return false; 
     }
+    
 }

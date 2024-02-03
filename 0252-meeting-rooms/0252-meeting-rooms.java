2dcf5d432c6f437de6eb37a13df8eb777373b558
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-03 16:16:05 -0700

    Time: 7 ms (32.80%), Space: 45.8 MB (63.15%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,11 +1,25 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); 
-        for(int i = 0; i < intervals.length - 1; i++) {
-            if(intervals[i][1] > intervals[i + 1][0]) {
-                return false;
+        Arrays.sort(intervals, new Comparator<int[]>() { // n log n (merge)
+            @Override
+            // Compare values according to columns
+            public int compare(final int[] entry1, final int[] entry2){
+                return entry1[0] - entry2[0];    
             }
+        }); 
+        
+        int size = intervals.length; 
+        
+        for(int i = 0; i < size - 1; i++) {
+            int currentEnd = intervals[i][1]; // end 
+            int nextStart = intervals[i + 1][0];
+              if( currentEnd > nextStart ){
+                return false;
+              }
         }
-        return true;
+
+        return true; 
+        
     }
+    // map 
 }

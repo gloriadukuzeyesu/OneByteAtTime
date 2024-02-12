e76f4015deaf5427e94b6a4da0b2b67ef67e8848
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-12 16:12:55 -0700

    Time: 1 ms (90.23%), Space: 41.2 MB (19.40%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,41 +1,13 @@
-// class Solution {
-//     public int reverse(int x) {
-//         //123
-//         if(x == 0) return 0;
-//         if(x > Integer.MAX_VALUE && x < Integer.MIN_VALUE) return 0; 
-            
-//         boolean isFalse = false;
-        
-//         if(x < 0) {
-//             isFalse = true; 
-//             x = -1 * x;
-//         }
-//         int res = 0;
-//         while ( x != 0 ) {
-//             res =  res * 10 + (x % 10);
-//             x = x / 10;   
-//         }
-        
-        
-//         if(isFalse) {
-//             res = -res;
-
-//         }
-        
-//         return res; 
-//     }
-// }
-
 class Solution {
     public int reverse(int x) {
-        int rev = 0;
-        while (x != 0) {
-            int pop = x % 10;
-            x /= 10;
+        int rev = 0; 
+        while(x != 0) {
+           int pop = x % 10; 
+            x /= 10;   
             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            rev = rev * 10 + pop;
+            rev = rev * 10 + pop; 
         }
-        return rev;
+        return rev; 
     }
 }

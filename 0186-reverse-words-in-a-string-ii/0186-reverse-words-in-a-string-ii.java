10c0adc222624637f1323c0e4ac8fea66f6eed7f
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-15 19:21:46 -0700

    Time: 1 ms (99.38%), Space: 48.4 MB (80.85%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
@@ -1,54 +1,22 @@
 class Solution {
-    
-    public void reverseWords(char[] s) {
-            
-    // reverseString(s, 0, s.length-1);
-    helper(s, 0, s.length-1);
-    int l=0;
 
-          
-   for(int i=0; i < s.length; i++){
-       if(s[i] == ' '){
-           // reverseString(s, l, i-1);
-           helper(s, l, i -1);
-           l = i+1;
+    public void reverseWords(char[] s) {
+       helper(s, 0, s.length-1);
+       int l= 0;   
+       for(int r= 0; r < s.length; r++){
+           if(s[r] == ' '){
+               helper(s, l, r -1);
+               l = r + 1;
+           }
        }
-   }
-   // reverseString(s, l, s.length-1);
-        helper( s, l, s.length - 1); 
+        helper( s, l, s.length - 1);  
+    }
     
-}
- 
-//   public void reverseString(char [] s, int l, int h){
-    
-//     while( l < h){
-//         char temp = s[l];
-//         s[l++] = s[h];
-//         s[h--]= temp;
-//     }
-// }
-    
-    
-    
-//     public void reverseWords(char[] s) {
-//         helper(0,s.length - 1,s);         
-//         int l = 0; 
-        
-//         for(int r = 0; r < s.length; r++) {
-//             if(s[r] == ' ') {
-//                 helper(l, r - 1, s); 
-//                 l = r + 1; 
-//             } 
-//         }
-        
-//         helper(l, s.length - 1, s); 
-//     }
-    
-    public void helper(char[] s, int a, int b) {
-        while( a < b) {
-            char temp = s[a]; 
-            s[a++] = s[b]; 
-            s[b--] = temp; 
+    public void helper(char[] s, int l, int r) {
+        while( l < r) {
+            char temp = s[l]; 
+            s[l++] = s[r]; 
+            s[r--] = temp; 
         } 
     }
 }

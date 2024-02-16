328407704a0a379148e06a7f781c932bc336462a
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-15 19:18:44 -0700

    Time: 1 ms (99.38%), Space: 48.5 MB (72.11%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
@@ -2,34 +2,36 @@ class Solution {
     
     public void reverseWords(char[] s) {
             
-    reverseString(s, 0, s.length-1);
+    // reverseString(s, 0, s.length-1);
+    helper(s, 0, s.length-1);
     int l=0;
 
           
    for(int i=0; i < s.length; i++){
        if(s[i] == ' '){
-           reverseString(s, l, i-1);
+           // reverseString(s, l, i-1);
+           helper(s, l, i -1);
            l = i+1;
        }
    }
-   reverseString(s, l, s.length-1);
+   // reverseString(s, l, s.length-1);
+        helper( s, l, s.length - 1); 
     
 }
  
-  public void reverseString(char [] s, int l, int h){
+//   public void reverseString(char [] s, int l, int h){
     
-    while( l < h){
-        char temp = s[l];
-        s[l++] = s[h];
-        s[h--]= temp;
-    }
-}
+//     while( l < h){
+//         char temp = s[l];
+//         s[l++] = s[h];
+//         s[h--]= temp;
+//     }
+// }
     
     
     
 //     public void reverseWords(char[] s) {
-//         int i = 0, j = s.length - 1; 
-//         helper(i,j,s);         
+//         helper(0,s.length - 1,s);         
 //         int l = 0; 
         
 //         for(int r = 0; r < s.length; r++) {
@@ -42,14 +44,11 @@ class Solution {
 //         helper(l, s.length - 1, s); 
 //     }
     
-//     public void helper(int i, int j, char[] s) {
-//         System.out.println(Arrays.toString(s)); 
-//         while( i < j) {
-//             char temp = s[i]; 
-//             s[i] = s[j]; 
-//             s[j] = temp; 
-//             i++; 
-//             j--; 
-//         } 
-//     }
+    public void helper(char[] s, int a, int b) {
+        while( a < b) {
+            char temp = s[a]; 
+            s[a++] = s[b]; 
+            s[b--] = temp; 
+        } 
+    }
 }

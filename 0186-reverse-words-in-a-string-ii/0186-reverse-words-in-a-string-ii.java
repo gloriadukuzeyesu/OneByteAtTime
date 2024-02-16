7e41591afae407aa1214ffdb252e0884b418ea3f
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-15 19:10:20 -0700

    Time: 1 ms (99.38%), Space: 48.5 MB (72.11%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
@@ -1,26 +1,55 @@
 class Solution {
+    
     public void reverseWords(char[] s) {
-//       reverse the whole sentence
-      reverse(s, 0,s.length - 1); 
-      int start = 0;
-      // reverse every single word
-      for(int i = 0; i < s.length; i++) {
-        if(s[i] == ' ') {
-          reverse(s, start, i - 1);
-          start = i + 1; 
-        }
-      }
-//       revese the last word
-      reverse(s, start, s.length - 1);
-      
+            
+    reverseString(s, 0, s.length-1);
+    int l=0;
+    int h= s.length-1;
+          
+   for(int i=0; i < s.length; i++){
+       if(s[i] == ' '){
+           reverseString(s, l, i-1);
+           l = i+1;
+       }
+   }
+   reverseString(s, l, s.length-1);
+    
+}
+ 
+  public void reverseString(char [] s, int l, int h){
+    
+    while( l < h){
+        char temp = s[l];
+        s[l++] = s[h];
+        s[h--]= temp;
     }
-    private  void reverse (char[] arr, int s, int e) {
-      while( e < arr.length && s < e) {
-        char temp = arr[s];
-        arr[s] = arr[e];
-        arr[e] = temp; 
-        s++;
-        e--; 
-      }
-    }
+}
+    
+    
+    
+//     public void reverseWords(char[] s) {
+//         int i = 0, j = s.length - 1; 
+//         helper(i,j,s);         
+//         int l = 0; 
+        
+//         for(int r = 0; r < s.length; r++) {
+//             if(s[r] == ' ') {
+//                 helper(l, r - 1, s); 
+//                 l = r + 1; 
+//             } 
+//         }
+        
+//         helper(l, s.length - 1, s); 
+//     }
+    
+//     public void helper(int i, int j, char[] s) {
+//         System.out.println(Arrays.toString(s)); 
+//         while( i < j) {
+//             char temp = s[i]; 
+//             s[i] = s[j]; 
+//             s[j] = temp; 
+//             i++; 
+//             j--; 
+//         } 
+//     }
 }

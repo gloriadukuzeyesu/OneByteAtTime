0477df4e3b23c87a4336018643727a819f4d5ec6
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-17 12:34:23 -0700

    Time: 6 ms (6.21%), Space: 44.4 MB (51.33%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,17 +14,49 @@
  * }
  */
 class Solution {
+    
     public int kthSmallest(TreeNode root, int k) {
+        
+        /*
+        1. minHeap => descreasing order. root node has the minimum value in the tree
+        priority queue => order elements based on natual ordering and using comparator
+        first in first out. 
+        sort as we go. by putting nodes in the the heap. 
+        */
+        // int counter = 0; 
         List<Integer> list = new ArrayList<>(); 
-        dfs(root, list); 
-        return list.get(k - 1); 
+        
+        // visit left 
+       // recursive(root.left, counter, list);
+        
+        recursive(root, list); 
+        
+//         if(counter < k) {
+//             // if the k number is not found visit right
+//             recursive(root.right, counter, list); 
+//             System.out.println("list: " + list);
+//         }
+        
+// //         // return the k smalles. 
+// //         for (int i = 0; i < k; i++) {
+            
+// //         }
+        System.out.println(list);
+        return list.get(k - 1);
+        
     }
-    public void dfs(TreeNode root, List<Integer> list) {
+    
+    public void recursive(TreeNode root,List<Integer> list) {
+        // base case
         if(root == null) {
-            return;
+           return; 
         }
-        dfs(root.left, list); 
+   
+        // recursive case
+        recursive(root.left,list);
         list.add(root.val); 
-        dfs(root.right, list); 
+        recursive(root.right,list);
+        
+    
     }
 }

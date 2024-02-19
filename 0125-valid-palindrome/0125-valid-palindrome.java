ccca8df81303d2c000946472effd8804a1f3ba2a
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-19 16:09:27 -0700

    Time: 3 ms (76.35%), Space: 42.9 MB (76.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,19 @@
 class Solution {
     
     public boolean isPalindrome(String s) {
-        
+        s = s.toLowerCase(); 
         int left = 0, right = s.length() - 1; 
-        
         while( left < right) {
              // remove spacesnon-alphanumeric characters
-            
             while( !Character.isLetterOrDigit( s.charAt(left)) && left < right) {
                 left++; 
             }
             while( !Character.isLetterOrDigit(s.charAt(right)) && left < right) {
                 right--; 
             }
-            // convert all to lower cases
-            if( Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            if(s.charAt(left) != s.charAt(right)) {
                 return false; 
-            } 
+            }
             left++; 
             right--; 
         }           

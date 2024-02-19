6f48670389c24c9975ad67a1bf55c64f87c7cb73
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-19 16:23:47 -0700

    Time: 17 ms (22.26%), Space: 44.6 MB (29.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,31 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) {
-            return false; 
-        }
-        Map<Character, Integer> sMap = new HashMap<>(); 
-        Map<Character, Integer> tMap = new HashMap<>(); 
+        if(s.length() != t.length()) return false; 
+//         int[] s_array = new int[s.length()]; 
+//         int[] t_array = new int[t.length()]; 
         
+//         for(int i = 0; i < s.length(); i++) {
+//             s_array[s.charAt(i) -'a']++; 
+//             t_array[s.charAt(i) -'a']--; 
+//         }
+        
+//         for(int i = 0; i < t.length(); i++) {
+//             s_array[t.charAt(i) - 'a']--; 
+//         }
+//         for(int i = 0; i < s.length(); i++) {
+//             if(s_array[i] != 0) {
+//                 return false; 
+//             }
+//         }
+        
+        Map<Character, Integer> map = new HashMap<>(); 
         for(int i = 0; i < s.length(); i++) {
-            if(sMap.containsKey(s.charAt(i))) {
-                // update its value 
-                int newValue = sMap.get(s.charAt(i)) + 1; 
-                sMap.put(s.charAt(i), newValue);
-            } else {
-                sMap.put(s.charAt(i), 1);
-            }  
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1); 
         }
-        
         for(int i = 0; i < t.length(); i++) {
-            if(tMap.containsKey(t.charAt(i))) {
-                // update its value 
-                int newValue = tMap.get(t.charAt(i)) + 1; 
-                tMap.put(t.charAt(i), newValue);
-            } else {
-                tMap.put(t.charAt(i), 1);
-            }  
+            if(!map.containsKey(t.charAt(i))) {
+                return false; 
+            } else{
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1); 
+            }
         }
-        // compare two maps.
-        return sMap.equals(tMap); 
+        for(char c : map.keySet()) {
+            if(map.get(c) != 0) {
+                return false; 
+            }
+        }
+        return true; 
+        
     }
 }

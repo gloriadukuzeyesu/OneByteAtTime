64e69f9572a57b4101623105be1306f3eacfd745
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-19 16:26:03 -0700

    Time: 6 ms (46.47%), Space: 42.8 MB (91.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,36 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false; 
-//         int[] s_array = new int[s.length()]; 
-//         int[] t_array = new int[t.length()]; 
-        
-//         for(int i = 0; i < s.length(); i++) {
-//             s_array[s.charAt(i) -'a']++; 
-//             t_array[s.charAt(i) -'a']--; 
-//         }
-        
-//         for(int i = 0; i < t.length(); i++) {
-//             s_array[t.charAt(i) - 'a']--; 
-//         }
-//         for(int i = 0; i < s.length(); i++) {
-//             if(s_array[i] != 0) {
-//                 return false; 
-//             }
-//         }
-        
-        Map<Character, Integer> map = new HashMap<>(); 
+        int[] counter = new int[26]; 
         for(int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1); 
+            counter[s.charAt(i) -'a']++; 
+            counter[t.charAt(i) -'a']--;    
         }
-        for(int i = 0; i < t.length(); i++) {
-            if(!map.containsKey(t.charAt(i))) {
-                return false; 
-            } else{
-                map.put(t.charAt(i), map.get(t.charAt(i)) - 1); 
-            }
-        }
-        for(char c : map.keySet()) {
-            if(map.get(c) != 0) {
+        
+        for(int count : counter) {
+            if(count != 0) {
                 return false; 
             }
         }

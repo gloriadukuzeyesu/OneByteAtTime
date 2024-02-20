97827bd278c3fd6073ab501430eb7eacd6f0f3a2
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-19 17:29:49 -0700

    Time: 0 ms (100.00%), Space: 44.3 MB (55.90%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,13 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false; 
+        
+        if(head == null || head.next == null) {
+            return false; 
+        }
+        
         ListNode slow = head; 
-        ListNode fast = head;
+        ListNode fast = head; 
+        
         while(fast != null && fast.next != null) {
             slow = slow.next; 
             fast = fast.next.next;
-            if(slow == fast){
+            
+            if(slow == fast) {
                 return true; 
             }
         }

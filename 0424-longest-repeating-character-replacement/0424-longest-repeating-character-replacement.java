8326ac94ffdb4828beccf90e79c794855e071e84
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-02-20 09:27:28 -0700

    Time: 10 ms (67.27%), Space: 42.5 MB (68.24%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,7 +1,5 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        
-        
         int[] frequency = new int[26]; 
         int result = 0; 
         int left = 0; 
@@ -19,30 +17,5 @@ class Solution {
             result = Math.max(result, right - left + 1);
         }
         return result;
-        
-        /*
-           int[] arr = new int[26];
-        int ans = 0;
-        int max = 0;
-        int i = 0;
-        for (int j = 0; j < s.length(); j++) {
-            arr[s.charAt(j) - 'A']++;
-            max = Math.max(max, arr[s.charAt(j) - 'A']);
-            if (j - i + 1 - max > k) {
-                arr[s.charAt(i) - 'A']--;
-                i++;
-            }
-            ans = Math.max(ans, j - i + 1);
-        }
-        return ans;
-        
-        
-        
-        
-        */
-        
-     
-        
-        
     }
 }

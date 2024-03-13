84716c42e947e4d441d77eaccc38c16f37aff386
Gloria Dukuzeyesu <93447505+gloriadukuzeyesu@users.noreply.github.com>
2024-03-12 19:36:02 -0600

    Time: 55 ms (82.58%), Space: 17.7 MB (61.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-      track_map = {}
-      for i, num in enumerate(nums):
-        diff = target - num
-        if diff in track_map:
-          return [track_map[diff], i]
-        track_map[num] = i
+        dict_nums = {}
+        result = []
         
-      return None
+        for i in range(len(nums)):
+            dict_nums[nums[i]] = i
         
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dict_nums and dict_nums[complement] != i:
+                result.append(i)
+                result.append(dict_nums[complement])
+                return result
